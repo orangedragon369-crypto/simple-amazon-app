dd0b11a28d24cbca513fe529b68d259098695106
Benjamin Ludington <orangedragon369@gmail.com>
2025-10-15 10:23:48 -0600

    adding cart item html

--- amazon/scripts/cart_orderHTML.mjs
+++ amazon/scripts/cart_orderHTML.mjs
@@ -1,1 +1,49 @@
 import {cart} from "./cart.mjs";
+import {} from "./items.mjs";
+import {addDays} from "./date-fns";
+
+let shipping = [0, 4.99, 9.99];
+const dateNTime = [formatRelative(subDays(new Date(), 6), new Date()), formatRelative(subDays(new Date(), 4), new Date()), formatRelative(subDays(new Date(), 2), new Date())];
+function fillCart (){
+    const cartCollect = document.getElementById("cart-collection");
+    for (item in cart.items){
+        cartCollect.innerHTML += `
+                    <div class="item-box cont">
+                        <img src="${cart.pics[item]}" width="175px">
+                        <div class="ainer space">
+                            <b>${cart.items[item]}</b>
+                            <div>${cart.prices[item]}</div>
+                            <div><span>Quantity: ${cart.num[items]}</span><button onclick="cart.delete()"></button></div>
+                        </div>
+                        <div class="ainer space">
+                            <b>Choose a delivery option:</b>
+                            <form>
+                                <input type="radio" id="good" name="Shipping" value="HTML">
+                                <label for="good">
+                                    <span>${dateNTime[0]}</span>
+                                    <div>&emsp;&ensp;Free Shipping($${shipping[0]})</div>
+                                </label><br>
+                                <input type="radio" id="better" name="Shipping" value="CSS">
+                                <label for="better">
+                                    <span>${dateNTime[1]}</span>
+                                    <div>&emsp;&ensp;Express Shipping($${shipping[1]})</div>
+                                </label><br>
+                                <input type="radio" id="best" name="Shipping" value="JavaScript">
+                                <label for="best">
+                                    <span>${dateNTime[2]}</span>
+                                    <div>&emsp;&ensp;Express+ Shipping($${shipping[2]})</div>
+                                </label>
+                            </form>
+                        </div>
+                    </div>`
+    }
+}
+
+function order(){
+    alert(`You have successfully order the contents of your cart, costing $${cart.total} in total.`);
+    cart.clear();
+}
+
+fillCart()
+const placeOrder = document.getElementById("order");
+placeOrder.addEventListener("onclick", order());
